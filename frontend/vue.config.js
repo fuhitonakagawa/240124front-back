8287be44f606d1bc1094c7e7f0bad23da7ea417e
fuhitonakagawa <132970394+fuhitonakagawa@users.noreply.github.com>
2024-01-24 17:10:24 +0900

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,8 +3,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    disableHostCheck: true,
-    allowedhosts: 'all',
+    allowedHosts: 'all',
   }
 });
 
